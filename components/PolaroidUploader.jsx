5d15d2984232ae48ed1a0fef7e871571e8ab5502
abhigk <abhishekgupta0595@gmail.com>
2024-12-22 12:39:52 +1100

    Show frame controls only if there are images

--- components/PolaroidUploader.jsx
+++ components/PolaroidUploader.jsx
@@ -69,23 +69,6 @@ const PolaroidUploader = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      {/* Frame Size Controls */}
-      <div className="mb-6 flex justify-center gap-4">
-        {Object.keys(FRAME_SIZES).map((size) => (
-          <button
-            key={size}
-            onClick={() => setFrameSize(size)}
-            className={`px-4 py-2 rounded-lg ${
-              frameSize === size
-                ? "bg-blue-500 text-white"
-                : "bg-gray-100 hover:bg-gray-200"
-            }`}
-          >
-            {size.charAt(0).toUpperCase() + size.slice(1)} Frame
-          </button>
-        ))}
-      </div>
-
       {/* Upload Area */}
       <Card
         className={`mb-8 ${isDragging ? "border-blue-500 bg-blue-50" : ""}`}
@@ -110,6 +93,25 @@ const PolaroidUploader = () => {
           </p>
         </CardContent>
       </Card>
+
+      {/* Frame Size Controls */}
+      {images.length > 0 && (
+        <div className="mb-6 flex justify-center gap-4">
+          {Object.keys(FRAME_SIZES).map((size) => (
+            <button
+              key={size}
+              onClick={() => setFrameSize(size)}
+              className={`px-4 py-2 rounded-lg ${
+                frameSize === size
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-100 hover:bg-gray-200"
+              }`}
+            >
+              {size.charAt(0).toUpperCase() + size.slice(1)} Frame
+            </button>
+          ))}
+        </div>
+      )}
 
       {/* Polaroid Gallery */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
