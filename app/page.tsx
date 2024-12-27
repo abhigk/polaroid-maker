65f2bd234e60c5604579ed0ef9e9085ed440d112
abhigk <abhishekgupta0595@gmail.com>
2024-12-28 10:06:16 +1100

    Add 3d model

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import HomePage from '@/components/HomePage'
-import PolaroidCanvas from '@/components/PolaroidCanvas'
 import ModelViewer from '@/components/ModelViewer'
 
 export default function Home() {
   return <HomePage />
-  // return <PolaroidCanvas />;
   // return <ModelViewer />;
 }
